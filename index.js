64b9dd6d92d00dec3d123147b533bc455dd3b6ce
JM [DELL] <jmanuel.rar@emeraldigital.com>
2019-10-10 13:46:19 -0500

    [MSSQL] Fourth (failed) attempt: Connection String

--- index.js
+++ index.js
@@ -2,30 +2,30 @@ require('dotenv').config()
 
 // Setting up a connection
 const { Sequelize, Model, DataTypes } = require('sequelize');
-
+const connectionString = `Driver={SQL Server Native Client 11.0};Server=localhost\\${process.env.DB_INSTANCE};Database=${process.env.DB_NAME};Trusted_Connection=yes;`
+console.log(connectionString);
 
 // Passing parameters separately (BEFORE: Set values in .env file)
 const Connection = new Sequelize({
   dialect: 'mssql',
   dialectModulePath: 'sequelize-msnodesqlv8',
-  server: process.env.DB_SERVER,
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  port: '16044',
-  loggin: true,
-  
+  //host: 'localhost',
+  //username: process.env.DB_USER,
+  //password: process.env.DB_PASS,
+  //database: process.env.DB_DATABASE,
+
+  dialectOptions: {
+    connectionString,
+    //driver: 'SQL Server Native Client 11.0',
+    //instanceName: process.env.DB_INSTANCE,
+    //trustedConnection: true
+  },
+
   pool: {
     min: 0,
     max: 5,
     idle: 10000
-  },
-
-  dialectOptions: {
-    instanceName: process.env.DB_INSTANCE,
-    encrypt: true,
-    trustedConnection: true
-  }
+  }  
 
 });
 
