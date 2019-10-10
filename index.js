250c4fcdada652f4c5ab622610e221004e48f473
JM [DELL] <jmanuel.rar@emeraldigital.com>
2019-10-10 13:56:07 -0500

    [MSSQL] Fifth (failed) attempt: Windows authentication

--- index.js
+++ index.js
@@ -2,31 +2,31 @@ require('dotenv').config()
 
 // Setting up a connection
 const { Sequelize, Model, DataTypes } = require('sequelize');
-const connectionString = `Driver={SQL Server Native Client 11.0};Server=localhost\\${process.env.DB_INSTANCE};Database=${process.env.DB_NAME};Trusted_Connection=yes;`
-console.log(connectionString);
+// const connectionString = `Driver={SQL Server Native Client 11.0};Server=localhost\\${process.env.DB_INSTANCE};Database=${process.env.DB_NAME};Trusted_Connection=yes;`
+// console.log(connectionString);
 
 // Passing parameters separately (BEFORE: Set values in .env file)
 const Connection = new Sequelize({
   dialect: 'mssql',
   dialectModulePath: 'sequelize-msnodesqlv8',
-  //host: 'localhost',
-  //username: process.env.DB_USER,
-  //password: process.env.DB_PASS,
-  //database: process.env.DB_DATABASE,
-
+ 
   dialectOptions: {
-    connectionString,
-    //driver: 'SQL Server Native Client 11.0',
-    //instanceName: process.env.DB_INSTANCE,
-    //trustedConnection: true
+    //connectionString,
+    driver: 'SQL Server Native Client 10.0',
+    instanceName: process.env.DB_INSTANCE,
+    trustedConnection: true
   },
 
   pool: {
     min: 0,
     max: 5,
     idle: 10000
-  }  
+  },
 
+  host: 'localhost',
+  //username: process.env.DB_USER,
+  //password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
 });
 
 // Testing connection
