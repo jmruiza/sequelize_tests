7210c5e4f92e08f817a4ae924c9574af32745f4b
JM [DELL] <jmanuel.rar@emeraldigital.com>
2019-10-10 16:28:07 -0500

    [MSSQL] Seventh attempt (works): Use tedious base

--- index.js
+++ index.js
@@ -1,54 +1,147 @@
 require('dotenv').config()
 
-// Setting up a connection
-const { Sequelize, Model, DataTypes } = require('sequelize');
-// const connectionString = `Driver={SQL Server Native Client 11.0};Server=localhost\\${process.env.DB_INSTANCE};Database=${process.env.DB_NAME};Trusted_Connection=yes;`
-// console.log(connectionString);
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+var TYPES = require('tedious').TYPES;
+var async = require('async');
 
-// Passing parameters separately (BEFORE: Set values in .env file)
-const Connection = new Sequelize({
-  dialect: 'mssql',
-  dialectModulePath: 'sequelize-msnodesqlv8',
- 
-  dialectOptions: {
-    //connectionString,
-    driver: 'SQL Server Native Client 12.0',
-    instanceName: process.env.DB_INSTANCE,
-    //trustedConnection: true
+// Create connection to database
+var config = {
+  server: 'localhost',
+  authentication: {
+    type: 'default',
+    options: {
+      userName: process.env.DB_USER,
+      password: process.env.DB_PASS,
+    }
   },
+  options: {
+      //port: 16044,
+      database: process.env.DB_NAME,
+      instanceName: process.env.DB_INSTANCE
+  }
+}
 
-  host: '127.0.0.1',
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_DATABASE,
-  port: '16044',
-  server: process.env.DB_SERVER
-});
+var connection = new Connection(config);
 
-// Testing connection
-Connection.authenticate().then(() => {
-    console.log('Connection has been established successfully.');
-}).catch(err => {
-    console.error('Unable to connect to the database:', err);
-});
+function getVehicles(callback) {
+  console.log('Reading Vehicles...');
 
+  // Read all rows from table
+  request = new Request(
+    //'SELECT @@VERSION',
+    'SELECT VehicleID, VehicleName, Brand, Model, PlateNumber, EngineSerialNumber FROM orgVehicle WHERE DeletedOn IS NULL;',
+    function(err, rowCount, rows) {
+      if (err) {
+        console.log(err);
+        callback(err);
+      } else {
+        console.log(rowCount + ' row(s) returned');
+        callback(null);
+      }
+    }
+  );
 
-
-
-
-/*
-class User extends Model {}
-User.init({
-  username: DataTypes.STRING,
-  birthday: DataTypes.DATE
-}, { sequelize, modelName: 'user' });
-
-sequelize.sync()
-  .then(() => User.create({
-    username: 'janedoe',
-    birthday: new Date(1980, 6, 20)
-  }))
-  .then(jane => {
-    console.log(jane.toJSON());
+  // Print the rows read
+  var result = "";
+  request.on('row', function(columns) {
+      columns.forEach(function(column) {
+          if (column.value === null) {
+              console.log('NULL');
+          } else {
+              result += column.value + " ";
+          }
+      });
+      console.log(result);
+      result = "";
   });
-  */
+
+  // Execute SQL statement
+  connection.execSql(request);
+}
+
+function getEmployees(callback) {
+  console.log('Reading Employees...');
+
+  // Read all rows from table
+  request = new Request(
+    'SELECT E.EmployeeID, C.FirstName, C.LastNameFather, C.LastNameMother, P.PositionName FROM orgEmployee E JOIN orgContact C ON E.ContactID = C.ContactID JOIN orgEmployeePosition P ON E.PositionID = P.EmployeePositionID WHERE E.DeletedOn IS NULL;',
+    function(err, rowCount, rows) {
+      if (err) {
+        callback(err);
+      } else {
+        console.log(rowCount + ' row(s) returned');
+        callback(null);
+      }
+    }
+  );
+
+  // Print the rows read
+  var result = "";
+  request.on('row', function(columns) {
+      columns.forEach(function(column) {
+          if (column.value === null) {
+              console.log('NULL');
+          } else {
+              result += column.value + " ";
+          }
+      });
+      console.log(result);
+      result = "";
+  });
+
+  // Execute SQL statement
+  connection.execSql(request);
+}
+
+function getDepots(callback) {
+  console.log('Reading Depots...');
+
+  // Read all rows from table
+  request = new Request(
+    'SELECT DepotID, DepotName FROM orgDepot WHERE DeletedOn IS NULL;',
+    function(err, rowCount, rows) {
+      if (err) {
+        callback(err);
+      } else {
+        console.log(rowCount + ' row(s) returned');
+        callback(null);
+      }
+    }
+  );
+
+  // Print the rows read
+  var result = "";
+  request.on('row', function(columns) {
+      columns.forEach(function(column) {
+          if (column.value === null) {
+              console.log('NULL');
+          } else {
+              result += column.value + " ";
+          }
+      });
+      console.log(result);
+      result = "";
+  });
+
+  // Execute SQL statement
+  connection.execSql(request);
+}
+
+// Attempt to connect and execute queries if connection goes through
+connection.on('connect', function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Connected');
+
+    // Execute queries
+    async.waterfall([
+      getVehicles,
+      getEmployees,
+      getDepots
+    ])
+  }
+});
+
+// Make queries
